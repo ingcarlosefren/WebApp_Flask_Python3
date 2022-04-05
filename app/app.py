8100e032ec88216323dc9839f785853ec6c8254a
cefaWSL <ing.cefa@gmail.com>
2022-04-05 23:16:20 +0200

    mejorar la vista front

--- app/app.py
+++ app/app.py
@@ -6,9 +6,14 @@ app=Flask(__name__)
 @app.route('/')
 
 def index():
+    datos=['33 Años', 'Masculino', 'Avda Cabo Gata 121 1 3', 'ing.cefa@gmail.com']
     data = {
         'Titulo':'Index',
-        'Bienvenida':'Saludos Carlos!'
+        'Bienvenida':'Saludos',
+        'Nombre':'Carlos Efren Fernandez Abad',
+        'Datos':datos,
+        'numDatos':len(datos)
+
     }
     return render_template('index.html', data=data)
 
